4f48160d33978158b8d107361e76c0c2d53fcf14
nayangoria <77434516+nayangoria@users.noreply.github.com>
2026-05-25 16:47:52 +0530

    added EnableWebSecurity

--- src/main/java/com/devArenaBackend/config/SecurityConfig.java
+++ src/main/java/com/devArenaBackend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.devArenaBackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -13,6 +14,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
