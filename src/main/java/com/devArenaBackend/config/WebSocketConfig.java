14e2cba091c342d82f2873d72e6898665e5db99d
nayangoria <77434516+nayangoria@users.noreply.github.com>
2026-05-26 12:45:42 +0530

    updated webSockets

--- src/main/java/com/devArenaBackend/config/WebSocketConfig.java
+++ src/main/java/com/devArenaBackend/config/WebSocketConfig.java
@@ -29,7 +29,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173")
+                .setAllowedOrigins("http://localhost:5173",
+                        "https://dev-arena-frontend.onrender.com")
                 .withSockJS();
     }
 }
